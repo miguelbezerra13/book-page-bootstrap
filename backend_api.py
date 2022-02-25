0c35b312aac132f3aee8e2889a7abdf2f2759be8
miguelbezerra13 <miguel.bezerra13@gmail.com>
2022-02-25 21:51:06 +0000

    Backend API + Frontend Connection
    - Small style changes (scss file)
    - Adaptations to the HTML file to make the connection with the JavaScript
    - Development of the backend API to provide the data to be shown in the web page and other minor adaptations to the CORS policy
    - JavaScript file:
      - Addition of the books to the select
      - Button to get more information about the books
      - Inclusion of a disappearing error message when the first option is selected

--- backend_api.py
+++ backend_api.py
@@ -1,24 +1,39 @@
 import pandas as pd
 import json
 from fastapi import FastAPI
-
 from fastapi.middleware.cors import CORSMiddleware
+from urllib.parse import unquote
 
 # Load the dataset
 books = pd.read_excel('livros_lidos.xls')
 
 app = FastAPI()
 
-origins = ["http://localhost", "http://localhost:5500"]
+origins = ["http://localhost:5500", "http://127.0.0.1:5500"]
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["*"],
+    allow_methods=["GET"],
     allow_headers=["*"])
 
-# Send the list with all the books to 
+# Send the list with all the books to be options for the select
 @app.get('/get_all_books')
 def get_all_books():
     return json.dumps(books.Livro.values.tolist(), ensure_ascii=False)
+
+# Send the details of the selected book
+@app.get('/get_book_info/{book}')
+def get_book_info(book):
+    
+    # Index of the chosen book
+    book_index = books.loc[books.Livro==book].index[0]
+
+    # Dictionary to compile the information about the chosen book
+    book_info = {}
+
+    for col in ['Autor 1', 'Língua', 'Género']:
+        book_info[col] = books[col][book_index]
+
+    return json.dumps(book_info, ensure_ascii=False)
--- main.js
+++ main.js
@@ -1,3 +1,67 @@
+// Add the options to the select
+my_list=document.getElementById("book-select");
+
 fetch('http://127.0.0.1:8000/get_all_books')
     .then(response => response.json())
-    .then(data => console.log(data));
+    .then(data => {
+        const parsedJSON = JSON.parse(data);
+        parsedJSON.forEach(element => {
+            let option = document.createElement('option');
+            option.value = element;
+            option.innerHTML = element;
+            my_list.appendChild(option); 
+        });
+    });
+
+// Get the search button
+searchButton = document.getElementById('search-button');
+
+// Add an event listener for a click
+searchButton.addEventListener('click', getBookInfo);
+
+// Define the function to retrieve the book information
+function getBookInfo(){
+
+    bookChoice = document.getElementById('book-select').value
+
+    if (bookChoice === 'Please select a book') {
+        
+        // Show an error message on the screen
+        error = document.createElement('h4')
+        error.setAttribute('id', 'error')
+        error.innerText = 'Please select a book'
+        error.style.color = 'red'
+        error.style.paddingTop = '10px'
+
+        // Add it to the container below the book selector
+        document.querySelector('.book-selector-container').appendChild(error)
+
+        // Remove the error message after 3 seconds
+        window.setTimeout(removeErrorMsg, 3000);
+
+        function removeErrorMsg(){
+            document.getElementById("error").remove();
+        }
+        
+    } else {
+        fetch('http://127.0.0.1:8000/get_book_info/'+bookChoice)
+        .then(response => response.json())
+        .then(data => {
+            // Parse the JSON
+            const parsedBookInfo = JSON.parse(data);
+
+            // Get the elements to add the additional info
+            const bookTitle = document.getElementById('book-title')
+            const bookAuthor = document.getElementById('book-author')
+            const bookGenre = document.getElementById('book-genre')
+            const bookLanguage = document.getElementById('book-language')
+
+            // Update the elements
+            bookTitle.innerText += ' '+bookChoice
+            bookAuthor.innerText += ' '+parsedBookInfo['Autor 1']
+            bookGenre.innerText += ' '+parsedBookInfo['Género']
+            bookLanguage.innerText += ' '+parsedBookInfo['Língua']
+            
+        });
+    };
+}
